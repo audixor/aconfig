ac2c76b3567f0f68e42b130972b35295ee1220a7
Eric Jacksch <securityguy@users.noreply.github.com>
2024-12-11 11:29:09 -0500

    update licence

--- config.go
+++ config.go
@@ -1,6 +1,7 @@
-// Copyright (c) 2021-2024 Tenebris Technologies Inc.
-// Use of this source code is governed by the MIT license.
-// Please see the LICENSE file for details.
+//
+// Copyright (c) 2024 Tenebris Technologies Inc.
+// Please see the LICENSE file for details
+//
 
 package aconfig
 
